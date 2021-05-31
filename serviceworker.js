a50d3092d82ff736a02bdf3396168bdf189b8be3
Chijindu Julian <juliancrosss@gmail.com>
2021-05-31 20:19:16 +0100

    fixes to blank offline page

--- serviceworker.js
+++ serviceworker.js
@@ -1,24 +1,9 @@
-let version = 'v1:1:1';
+let version = 'v1.1.2';
 
-/*
-Copyright 2015, 2019, 2020 Google LLC. All Rights Reserved.
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-http://www.apache.org/licenses/LICENSE-2.0
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
-// Incrementing OFFLINE_VERSION will kick off the install event and force
-// previously cached resources to be updated from the network.
 const OFFLINE_VERSION = 1;
 const CACHE_NAME = "offline";
 // Customize this with a different URL if needed.
-const OFFLINE_URL = "offline.html";
+const OFFLINE_URL = "/offline.html";
 
 self.addEventListener("install", (event) => {
     event.waitUntil(
@@ -90,6 +75,7 @@ const assets = [
     "index.html",
     "about.html",
     "work.html",
+    "serviceworker.js",
     "/scss/styles.css",
     "/scripts/script.js",
     "/images/circles.svg",
